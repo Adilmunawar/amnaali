14d044c57b5c2244578844a09f55bb627f1e89c6
AdilMunawar <adilfromavengers@gmail.com>
2025-06-08 22:35:46 +0500

    transform class to 2.2 in profile card

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -117,7 +117,7 @@ export const About = () => {
             viewport={{ once: true }}
             className="flex justify-start lg:justify-start pl-8"
           >
-            <div className="transform scale-[1.8]">
+            <div className="transform scale-[2.2]">
               <ProfileCard
                 avatarUrl="/lovable-uploads/43fd4e87-d6c9-42fa-a776-492777f17353.png"
                 miniAvatarUrl="/lovable-uploads/43fd4e87-d6c9-42fa-a776-492777f17353.png"
